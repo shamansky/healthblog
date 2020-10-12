699566a651e6c90cb06f6ebd37fe0541fb9fada6
George Ciurescu <shubst3r@gmail.com>
2020-10-12 19:18:29 +0100

    site prototype

---
+++ src/pages/HomePage.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const HomePage = () => (
+  <React.Fragment>
+    <h1>Hello, welcome to my website</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.Ultricies mi eget
+      mauris pharetra et ultrices.Semper eget duis at tellus.Dolor purus non
+      enim praesent elementum facilisis leo vel fringilla.Urna duis convallis
+      convallis tellus.Sapien et ligula ullamcorper malesuada proin libero nunc
+      consequat interdum.Platea dictumst quisque sagittis purus.Sed elementum
+      tempus egestas sed sed risus pretium.Commodo ullamcorper a lacus
+      vestibulum.Enim tortor at auctor urna nunc id cursus metus.Viverra ipsum
+      nunc aliquet bibendum.Eu tincidunt tortor aliquam nulla facilisi cras
+      fermentum odio.Suspendisse sed nisi lacus sed viverra tellus in.Quam
+      viverra orci sagittis eu volutpat odio facilisis mauris sit. Scelerisque
+      purus semper eget duis at.Vulputate mi sit amet mauris commodo quis
+      imperdiet massa.Parturient montes nascetur ridiculus mus mauris vitae
+      ultricies leo.Etiam sit amet nisl purus.Et ultrices neque ornare aenean
+      euismod elementum.Sed id semper risus in hendrerit.In pellentesque massa
+      placerat duis ultricies lacus sed.Feugiat pretium nibh ipsum
+      consequat.Morbi enim nunc faucibus a pellentesque sit amet.Id volutpat
+      lacus laoreet non curabitur gravida.Ultrices gravida dictum fusce ut
+      placerat.Varius morbi enim nunc faucibus a pellentesque sit amet.Eget
+      nullam non nisi est sit.Et malesuada fames ac turpis egestas integer.
+      Blandit volutpat maecenas volutpat blandit aliquam etiam erat.Et molestie
+      ac feugiat sed lectus vestibulum mattis.Et malesuada fames ac turpis
+      egestas integer eget aliquet.Ridiculus mus mauris vitae ultricies leo
+      integer.Varius morbi enim nunc faucibus a pellentesque.Adipiscing elit ut
+      aliquam purus sit amet luctus venenatis lectus.Vel pretium lectus quam id
+      leo in vitae.Sed adipiscing diam donec adipiscing.Sollicitudin aliquam
+      ultrices sagittis orci a.Tincidunt arcu non sodales neque sodales
+      ut.Euismod elementum nisi quis eleifend quam adipiscing vitae.Scelerisque
+      mauris pellentesque pulvinar pellentesque habitant morbi tristique.Cursus
+      metus aliquam eleifend mi in nulla posuere. Molestie a iaculis at
+      erat.Cras ornare arcu dui vivamus arcu felis bibendum.Fermentum dui
+      faucibus in ornare quam viverra orci sagittis eu.Aliquam etiam erat velit
+      scelerisque in.Nisl purus in mollis nunc sed id.Nec nam aliquam sem et
+      tortor.Sodales ut eu sem integer.Faucibus a pellentesque sit amet
+      porttitor eget dolor morbi.Sagittis aliquam malesuada bibendum arcu
+      vitae.Fusce id velit ut tortor pretium.Vitae proin sagittis nisl rhoncus
+      mattis rhoncus urna neque viverra.Augue eget arcu dictum varius duis at.
+      Felis eget velit aliquet sagittis id consectetur purus.Vitae congue eu
+      consequat ac.Et netus et malesuada fames ac turpis egestas sed tempus.Id
+      venenatis a condimentum vitae.Eu sem integer vitae justo eget magna
+      fermentum iaculis eu.Tincidunt nunc pulvinar sapien et ligula
+      ullamcorper.Tempus quam pellentesque nec nam aliquam sem et tortor.Leo
+      duis ut diam quam.Turpis egestas integer eget aliquet nibh.Tempus
+      imperdiet nulla malesuada pellentesque elit eget gravida cum.Diam sit amet
+      nisl suscipit adipiscing bibendum.Id neque aliquam vestibulum
+      morbi.Egestas egestas fringilla phasellus faucibus scelerisque eleifend
+      donec pretium vulputate.Risus viverra adipiscing at in tellus integer
+      feugiat.Aliquet nibh praesent tristique magna.Orci porta non pulvinar
+      neque laoreet suspendisse interdum.Sagittis orci a scelerisque purus
+      semper.Lectus urna duis convallis convallis tellus id.
+    </p>
+  </React.Fragment>
+);
+
+export default HomePage;
